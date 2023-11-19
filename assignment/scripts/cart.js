b2d5ace20db68901c1e2297b52d7fea26b19c2e3
Jake Buroker <shamrecks@gmail.com>
2023-11-19 17:38:25 -0600

    completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,33 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+
+function addItem (item) {
+    basket.push(item);
+    return true;
+} 
+
+console.log(basket);
+addItem("1");
+console.log(basket);
 
 
+function listItems() {
+    for(var i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+    }
+}
+listItems();
 
 
+function empty() {
+    basket = [];
+}
+ 
 
-
-
-
-
+empty();
+console.log(basket);
 
 // DO NOT MODIFY
 // Used for automated testing
